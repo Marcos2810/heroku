edb40b7ac6da6f05e2b9a207890b765afa40823e
Marcos <marcos28_7@hotmail.com>
2019-08-10 12:05:56 +0200

    heroku
    no carga

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, SmartNinja!"
+    return render_template("index.html")
+
+@app.route("/aboutme") #para que acepte esta opcion
+@app.route("/about_me") #para que acepte esta opcion
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
